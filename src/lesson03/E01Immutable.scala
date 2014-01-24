e8615bcc9135d0ec267fb6739f889ee00f665761
Brutus <dev.brutus@gmail.com>
2014-01-24 18:09:02 +0400

    Immutables

--- src/lesson03/E01Immutable.scala
+++ src/lesson03/E01Immutable.scala
@@ -3,7 +3,17 @@ package lesson02
 /**
  *
  */
-object E01Immutable extends  App{
-  val a : List[String] = List("Hello", "world")
+object E01Immutable extends App {
+  val original: List[String] = List("Hello", "world")
+  val upperOriginal = original.map(_.toUpperCase)
+  println(upperOriginal)
+  println()
 
+  val evenNumbers = (1 to 20).filter(0 == _ % 2).map(n => s"Hello Mr. $n")
+  println(evenNumbers)
+
+  val evenNumbersWithSmth = evenNumbers.updated(0, "Foo")
+  println(evenNumbersWithSmth)
+  println(s"Is equals? ${evenNumbers == evenNumbersWithSmth}")
+  println()
 }
