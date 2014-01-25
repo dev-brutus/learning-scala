ae52e9528635e5780a37ea18864dda2a56466c2f
Brutus <dev.brutus@gmail.com>
2014-01-25 14:03:19 +0400

    Immutable

---
+++ src/lesson03/E06Immutable.scala
@@ -0,0 +1,13 @@
+package lesson03
+
+/**
+ *
+ */
+object E06Immutable extends App{
+  val originalVector = (1 to 10).toVector
+  
+  val modifiedVector = originalVector.updated(0, -1)
+
+  println(s"original: $originalVector")
+  println(s"modified: $modifiedVector")
+}
