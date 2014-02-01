b4129cf53595452475d9c2581744224d334c030c
Brutus <dev.brutus@gmail.com>
2014-02-02 00:43:52 +0400

    Info

--- src/lesson03/E11Info.scala
+++ src/lesson03/E11Info.scala
@@ -3,8 +3,16 @@ package lesson03
 /**
  *
  */
-object E11Info {
-  // contains
+object E11Info extends App {
+  val origin: IndexedSeq[String] = "Scala provides tail call optimization to allow for extensive use of recursion without stack overflow problems."
+
+  println("=== contains ===")
+  val containsScala = origin.contains("Scala")
+  val containsText = origin.contains("Text")
+  println(s"Contains `Scala': $containsScala")
+  println(s"Contains `Text': $containsText")
+  println
+
   // containsSlice
   // sameElements
   // corresponds
