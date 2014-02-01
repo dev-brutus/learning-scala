8fe3ce2f36f6e00e88a07c6669c254069bb86d2a
Brutus <dev.brutus@gmail.com>
2014-02-01 23:39:16 +0400

    Modification

--- src/lesson03/E10Modification.scala
+++ src/lesson03/E10Modification.scala
@@ -6,9 +6,22 @@ package lesson03
 object E10Modification extends App {
   val origin: IndexedSeq[String] = "In Scala all types inherit from a top-level class Any."
 
-  println("=== sliding ===")
+  println(s"Origin: $origin")
+  println
+
+  println("=== reverse ===")
+  println(s"Reverse order: ${origin.reverse}")
+  println
+
+  println("=== sorted ===")
+  println(s"Sorted order: ${origin.sorted}") // См. sortBy, sortWith
+  println
+
+  println("=== sliding, groupBy ===")
   val slided = origin.sliding(5).toList
+  val grouped = origin.grouped(5).toList
   println(s"Slided:  $slided")
+  println(s"Grouped: $grouped")
   println
 
   println("=== flatten, flattenMap ===")
@@ -24,10 +37,17 @@ object E10Modification extends App {
   println(s"FlatMapped: $flatMapped")
   println
 
+  println("=== groupBy ===")
+  val groupedByLength = origin.groupBy(_.length)
+  println(s"Grouped by length: $groupedByLength")
+  println
 
-  // groupBy
-  // grouped
-  // mkString
-  // reverse
-  // sorted, sortBy, sortWith
+  println("=== mkString, addString ===")
+  private val numbers = groupedByLength.keys
+  val numbersSimple = numbers.mkString(" ^ ")
+  val numbersComplex = numbers.addString(new StringBuilder, "Numbers (complex): ", " ^ ", ";")
+  println(s"Numbers (simple):  $numbersSimple;")
+  println(numbersComplex)
+  println
+
 }
