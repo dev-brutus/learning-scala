9ad3ed646bf857763d7348434f8b262f52fea8aa
Brutus <dev.brutus@gmail.com>
2014-02-01 23:18:25 +0400

    flatten

--- src/lesson03/E10Modification.scala
+++ src/lesson03/E10Modification.scala
@@ -3,15 +3,28 @@ package lesson03
 /**
  *
  */
-object E10Modification {
-/*  println("=== sliding ===")
-  val slided = origin.sliding(3).toList
+object E10Modification extends App {
+  val origin: IndexedSeq[String] = "In Scala all types inherit from a top-level class Any."
+
+  println("=== sliding ===")
+  val slided = origin.sliding(5).toList
   println(s"Slided:  $slided")
-  println*/
+  println
+
+  println("=== flatten, flattenMap ===")
+  private val f: String => Seq[Int] = s => {
+    new StringBuilder(s).map(c => c.toInt)
+  }
+  val mapped = origin.map(f)
+  val flattened = mapped.flatten
+  val flatMapped = origin.flatMap(f)
+
+  println(s"Mapped:     $mapped")
+  println(s"Flattened:  $flattened")
+  println(s"FlatMapped: $flatMapped")
+  println
 
 
-  // flatten
-  // flatMap
   // groupBy
   // grouped
   // mkString
