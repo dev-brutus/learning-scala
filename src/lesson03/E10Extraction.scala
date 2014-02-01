274c6c7f2fc5d15a5d7ed3a3b8ddb61b6297f8f6
Brutus <dev.brutus@gmail.com>
2014-02-01 22:35:34 +0400

    Extraction

--- src/lesson03/E10Extraction.scala
+++ src/lesson03/E10Extraction.scala
@@ -1,3 +1,4 @@
+
 package lesson03
 
 /**
@@ -6,17 +7,47 @@ package lesson03
 object E10Extraction extends App {
   val origin: IndexedSeq[String] = "Instead of the pseudo-type void, Scala has the actual singleton class Unit"
 
-  println(s"The 2nd element is \"${origin(2)}\"")
+  println(s"The 4th element is ${origin(4)}")
   println(s"This condition is always ${origin(3) == origin.apply(3)}")
+  println
 
-  // apply
-  // copyToArray
-  // copyToBuffer
-  // diff
-  // distinct
-  // drop, dropRight, dropWhile
-  // head, headOption, tail
-  // intersect
+  println("=== copyToArray ===")
+  val buffer: Array[String] = Array.fill(4)("")
+  origin.copyToArray(buffer, 2, 3) // См. copyToBuffer
+  println(s"The copied array: ${buffer.toList}")
+  println
+
+  println("=== diff, intersect ===")
+  private val strings = Array("Scala", "the")
+  val diff = origin.diff(strings)
+  val intersect = origin.intersect(strings)
+  println(s"Origin:         $origin")
+  println(s"Diff:           $diff")
+  println(s"Intersection:   $intersect")
+  println
+
+  println("=== distinct ===")
+  val distinct = origin.distinct
+  println(s"Origin:   $origin")
+  println(s"Distinct: $distinct")
+  println
+
+  println("=== drop ===")
+  val dropped = origin.drop(5) // См. drop, dropRight, dropWhile
+  println(s"Origin:  $origin")
+  println(s"Dropped: $dropped")
+  println
+
+  println("=== head, tail ===")
+  val head = origin.head
+  // См. headOption
+  val tail = origin.tail
+  println(s"Origin:  $origin")
+  println(s"Head: $head")
+  println(s"Tail: $tail")
+  println
+
+
   // padTo
   // partition
   // slice
