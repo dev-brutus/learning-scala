71879894655b525827f38e79a6a3dfdbd51bab62
Brutus <dev.brutus@gmail.com>
2014-02-01 23:43:16 +0400

    Small fixes

--- src/lesson03/package.scala
+++ src/lesson03/package.scala
@@ -2,9 +2,6 @@
  *
  */
 package object lesson03 {
-  type Num = lesson01.Num
-  implicit def doubleToNum(d: Double): Num = lesson01.Num(d)
-
   private val SPLIT_REGEXP = ",? "
 
   implicit def stringToSeq(s: String): List[String] = s.split(SPLIT_REGEXP).toList
