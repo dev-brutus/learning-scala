0114d2c7c4e976fa9ca58c7caeeb05948d1f821c
Brutus <dev.brutus@gmail.com>
2014-02-02 12:28:25 +0400

    Symbol

---
+++ src/lesson01/E12Symbol.scala
@@ -0,0 +1,41 @@
+package lesson01
+
+/**
+ *
+ */
+object E12Symbol extends App {
+  type Command = (Symbol, Int)
+
+  class Machine {
+    private val data = new Array[Int](255)
+    private var pos = 0
+
+    def print(from: Int, len: Int) = println(new String(data.slice(from, from + len).map(_.toChar)))
+
+    def process(command: Command): Unit = command match {
+      case ('add, x) => data(pos) = data(pos) + x
+      case ('sub, x) => data(pos) = data(pos) - x
+      case ('hi, x) => pos = (pos + x) % data.length
+      case ('lo, x) => pos = (pos - x) % data.length
+      case ('prn, x) => print(pos, x)
+    }
+  }
+
+  val machine = new Machine
+
+  List(
+    'add -> 0x68,
+    'hi -> 1,
+    'add -> 0x65,
+    'hi -> 1,
+    'add -> 0x6c,
+    'hi -> 1,
+    'add -> 0x6c,
+    'hi -> 1,
+    'add -> 0x6f,
+    'lo -> 5,
+    'prn -> 6
+  ).foreach(machine.process)
+
+
+}
