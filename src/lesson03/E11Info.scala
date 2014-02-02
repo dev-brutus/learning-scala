e1e6a3d5c2b346fd6439910d9b157d26ea2b5c52
Brutus <dev.brutus@gmail.com>
2014-02-02 13:55:51 +0400

    Info

--- src/lesson03/E11Info.scala
+++ src/lesson03/E11Info.scala
@@ -13,20 +13,67 @@ object E11Info extends App {
   println(s"Contains `Text': $containsText")
   println
 
-  // containsSlice
-  // sameElements
-  // corresponds
-  // count
-  // endsWith
-  // exists
-  // find
-  // forall
+  println("=== containsSlice ===")
+  val containsTailCall = origin.containsSlice(Array("tail", "call"))
+  val containsTailCallTo = origin.containsSlice(Array("tail", "call", "to"))
+  println(s"Contains `tail call':    $containsTailCall")
+  println(s"Contains `tail call to': $containsTailCallTo")
+  println
+
+  println("=== count, filter, forall ===")
+  val twoCharsPredicate: String => Boolean = _.length == 2
+  val count = origin.count(twoCharsPredicate)
+  val filtered = origin.filter(twoCharsPredicate)
+  // См. filterNot
+  val forAllOrigin = origin.forall(twoCharsPredicate)
+  val forAllFiltered = filtered.forall(twoCharsPredicate)
+  println(s"Filtered: $filtered")
+  println(s"Filtered size: ${filtered.length}; Count: $count")
+  println(s"forAll by origin:   $forAllOrigin")
+  println(s"forAll by filtered: $forAllFiltered")
+  println
+
+  println("=== startsWith, endsWith ===")
+  val startsWith = origin.startsWith(Array("Scala", "provides"))
+  val endsWith = origin.endsWith(Array("stack", "overflow", "problems."))
+  println(s"StartsWith: $startsWith")
+  println(s"EndsWith:   $endsWith")
+  println
+
+  println("=== exists, indexOf ===")
+
+  def findFunction(collection: Seq[String], what: String): (Boolean, Int) = {
+    collection.exists(_ == what) -> collection.indexOf(what)
+  }
+
+  val findYes = findFunction(origin, "Scala")
+  val findNo = findFunction(origin, "13")
+  println(s"Scala: $findYes")
+  println(s"13:    $findNo")
+  println
+
+  println("=== max, min, product, sum ===")
+  val numbers = Array(-2, -3, 10, 5)
+  println(s"Numbers: ${numbers.mkString(", ")}")
+  println(s"max: ${numbers.max}")
+  println(s"min: ${numbers.min}")
+  println(s"sum: ${numbers.sum}")
+  println(s"prd: ${numbers.product}")
+  println
+
+  println("=== corresponds ===")
+  val numbersStr = Array("-2", "-3", "10", "5")
+  val isCorresponded = numbers.corresponds(numbersStr)(_.toString == _)
+  println(s"isCorresponded: $isCorresponded")
+  println
+
+  println("=== segmentLength ===")
+  val oPredicate: String => Boolean = _.contains("o")
+  val segmentLength = origin.segmentLength(oPredicate, 10)
+  println(s"The longest sequence of words with letter `o' is: $segmentLength")
+  println(origin.zipWithIndex.map(s => (s._2, oPredicate(s._1), s._1)))
+  println
+
   // hasDefiniteSize, isTraversableAgain
-  // indexOf
   // isEmpty, nonEmpty
-  // lift
-  // max, min
-  // product, sum
-  // segmentLength
-  // startsWith
 }
